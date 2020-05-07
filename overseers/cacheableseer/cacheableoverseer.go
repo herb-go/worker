1af6eab32c003ced889537786d64701bc5f2eb86
jarlyyn <jarlyyn@qq.com>
2020-05-08 00:55:09 +0800

    update

---
+++ overseers/cacheableseer/cacheableoverseer.go
@@ -0,0 +1,18 @@
+package cacheableoverseer
+
+import "github.com/herb-go/worker"
+
+//Config overseer config struct
+type Config struct {
+}
+
+//Apply apply config to overseer
+func (c *Config) Apply(o *worker.PlainOverseer) error {
+	o.WithIntroduction("Cacheable workers")
+	return nil
+}
+
+//New create new config
+func New() *Config {
+	return &Config{}
+}
