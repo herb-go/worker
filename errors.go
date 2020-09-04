2c404d51f2435ef5b1550f2c6da86002c6c9d4d2
jarlyyn <jarlyyn@qq.com>
2020-09-04 20:02:25 +0800

    update

--- errors.go
+++ errors.go
@@ -1,8 +1,15 @@
 package worker
 
-import "errors"
+import (
+	"errors"
+	"fmt"
+)
 
 //ErrUnknownCommand error raised if given command is unknown.
 var ErrUnknownCommand = errors.New("worker:unknow command")
 
 var ErrWorkerNotFound = errors.New("worker not found")
+
+func NewWorkerNotFounderError(id string) error {
+	return fmt.Errorf("%w [%s]", ErrWorkerNotFound, id)
+}
